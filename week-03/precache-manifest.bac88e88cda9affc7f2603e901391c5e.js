c843d27239a4d120fb34d4120f67b00045d2ad5b
diamond520 <diamond.cc@gmail.com>
2019-08-04 18:45:57 +0800

    adjust display width

---
+++ week-03/precache-manifest.bac88e88cda9affc7f2603e901391c5e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2af25c03f7dee7385dff7ab11b4844a",
+    "url": "img/menu-btn.e2af25c0.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "76b2fff4ad8c74d9157c89254c44c1ec",
+    "url": "img/nav-btn-3.76b2fff4.svg"
+  },
+  {
+    "revision": "4404727972f397f5bad6",
+    "url": "js/app.df3e7beb.js"
+  },
+  {
+    "revision": "32632b299bb23a9aab65",
+    "url": "js/chunk-vendors.275ae397.js"
+  },
+  {
+    "revision": "7786b02e5bc5dc816631b1ece629bc57",
+    "url": "img/nav-btn-2.7786b02e.svg"
+  },
+  {
+    "revision": "e058b073c44e295ed845",
+    "url": "js/about.8b62cac6.js"
+  },
+  {
+    "revision": "061e25a5e1b222de2194fe55f338b6b9",
+    "url": "index.html"
+  },
+  {
+    "revision": "88cf096a85ce5fcee6d6644646721a00",
+    "url": "img/play-btn.88cf096a.svg"
+  },
+  {
+    "revision": "cd6e9b268248d74c944942bc8ef3f9b1",
+    "url": "img/nav-btn-1.cd6e9b26.svg"
+  },
+  {
+    "revision": "18198d2bf95850c4e88b4aea5248ed8c",
+    "url": "img/btn_random.18198d2b.svg"
+  },
+  {
+    "revision": "36e574dbe17ebe752c87928016ef13e9",
+    "url": "img/btn_next-track.36e574db.svg"
+  },
+  {
+    "revision": "5dd664c132bae52c7a848df63f3e7d24",
+    "url": "img/btn_last-track.5dd664c1.svg"
+  },
+  {
+    "revision": "fadba41e3b57c64d8837e42004457879",
+    "url": "img/btn_cycle.fadba41e.svg"
+  },
+  {
+    "revision": "65fa4c8800e8c718a65189cbd74172b4",
+    "url": "img/back-btn.65fa4c88.svg"
+  },
+  {
+    "revision": "ef39d7c0e3d72dcf5bab56df8eebb931",
+    "url": "img/abbeyroad-cover.ef39d7c0.jpg"
+  },
+  {
+    "revision": "4404727972f397f5bad6",
+    "url": "css/app.64a85506.css"
+  }
+];
--- week-03/service-worker.js
+++ week-03/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9932e04da11227c1f5585b21689bda9e.js"
+  "precache-manifest.bac88e88cda9affc7f2603e901391c5e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mp3-player"});
